484c8324abb02094bd0a738a2b6657c344187fe3
Ciprian Balea <ciprian.balea@threepillarglobal.com>
2012-09-13 10:08:42 +0300

    run only one test again

--- src/testFirstSeleniumTest.py
+++ src/testFirstSeleniumTest.py
@@ -44,51 +44,52 @@ class FirstTest(unittest.TestCase):
         finally:
             driver.quit()
     
-    def test_1(self):
-        self.googlePage()
-    
-    def test_2(self):
-        self.googlePage()
-        
-    def test_3(self):
-        self.googlePage()
+#    def test_1(self):
+#        self.googlePage()
+#    
+#    def test_2(self):
+#        self.googlePage()
+#        
+#    def test_3(self):
+#        self.googlePage()
     
     def test_4(self):
         self.googlePage()
     
-    def test_6(self):
-        seleniumHub = 'http://ec2-204-236-220-204.compute-1.amazonaws.com:4445/wd/hub'
-        desiredCapabilities = {"browserName":"firefox"}
-        browserProfile = None
-        driver = WebDriver(seleniumHub, desiredCapabilities, browserProfile)
-        
-        print "Running test on hub: %s" %seleniumHub
-        
-        # go to the google home page
-        driver.get("http://www.google.com")
+#    def test_6(self):
+#        seleniumHub = 'http://ec2-204-236-220-204.compute-1.amazonaws.com:4445/wd/hub'
+#        seleniumHub = 'http://localhost:4444/wd/hub'
+#        desiredCapabilities = {"browserName":"firefox"}
+#        browserProfile = None
+#        driver = WebDriver(seleniumHub, desiredCapabilities, browserProfile)
+#        
+#        print "Running test on hub: %s" %seleniumHub
+#        
+#        # go to the google home page
+#        driver.get("http://www.google.com")
+#    
+#        # find the element that's name attribute is q (the google search box)
+#        inputElement = driver.find_element_by_name("q")
+#        
+#        # type in the search
+#        inputElement.send_keys("Cheese!")
+#        
+#        # submit the form (although google automatically searches now without submitting)
+#        inputElement.submit()
+#        
+#        # the page is ajaxy so the title is originally this:
+#        print driver.title
+#        
+#        try:
+#            # we have to wait for the page to refresh, the last thing that seems to be updated is the title
+#            WebDriverWait(driver, 10).until(lambda driver : driver.title.lower().startswith("cheese!"))
+#        
+#            # You should see "cheese! - Google Search"
+#            print driver.title
+#            self.assertEquals(driver.title.index("aaa"), 0, "The expected page was not found!")
+#        
+#        finally:
+#            driver.quit()
     
-        # find the element that's name attribute is q (the google search box)
-        inputElement = driver.find_element_by_name("q")
-        
-        # type in the search
-        inputElement.send_keys("Cheese!")
-        
-        # submit the form (although google automatically searches now without submitting)
-        inputElement.submit()
-        
-        # the page is ajaxy so the title is originally this:
-        print driver.title
-        
-        try:
-            # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-            WebDriverWait(driver, 10).until(lambda driver : driver.title.lower().startswith("cheese!"))
-        
-            # You should see "cheese! - Google Search"
-            print driver.title
-            self.assertEquals(driver.title.index("aaa"), 0, "The expected page was not found!")
-        
-        finally:
-            driver.quit()
-    
-    def test_5(self):
-        self.googlePage()
+#    def test_5(self):
+#        self.googlePage()
